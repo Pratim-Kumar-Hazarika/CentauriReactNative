a7863ce986cc7d78c2f9f21f57f84ffac5c22b2a
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2022-01-09 12:29:25 +0530

    update : character count assignment

--- App.tsx
+++ App.tsx
@@ -1,114 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
+import React, { useState } from 'react';
+import {StyleSheet, View,Text,TextInput} from 'react-native';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  const [inputText,setInputText] = useState<Number>(20);
+  const [inputBoxBgColor,setInputBoxBgColor] = useState<string>('grey');
+  function inputHandler(e: string | React.SetStateAction<any> ){
+    if (e.length === 0){
+      setInputText(20);
+      setInputBoxBgColor('grey');
+    } else if (e.length !== 0){
+      setInputText(()=>20 - e.length);
+       if (e.length > 10 || e.length === 20 ){
+        setInputBoxBgColor('yellow');
+      } if (e.length < 10){
+        setInputBoxBgColor('grey');
+      } if ( e.length > 20){
+        setInputBoxBgColor('red');
+      }
+    }
+  }
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container} >
+      <Text>Character Count</Text>
+      <TextInput style={{
+         borderWidth:3,
+        borderColor:inputBoxBgColor,
+        padding:8,
+        width:200,
+        height:200,
+        borderRadius:10,
+      }}
+       multiline={true}
+      numberOfLines={10}
+      onChangeText={(e)=>inputHandler(e)}
+    />
+    <Text>{inputText} characters remaining</Text>
+  </View>
   );
 };
 
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container:{
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center',
   },
 });
 
