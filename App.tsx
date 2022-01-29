9992cf062b8fd2677228b3fd0edf6fd4eb43546d
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2022-01-30 00:23:35 +0530

    refactor

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-/* eslint-disable eslint-comments/no-unused-disable */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable prettier/prettier */
+/* eslint-disable */
 
 import React from 'react';
 import {
   Button,
+  FlatList,
+  Image,
+  ScrollView,
   StyleSheet,
   Text,
   View,
@@ -13,38 +13,43 @@ import {
 
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome'; 
+import { Data } from './utils/data';
+import Products from './components/Products';
 
-const FirstScreen = ({ navigation }:any) => {
-  return (
-    <View>
-    <Button
-      title="Click here to go to Seconed Screen"
-      onPress={() =>
-        navigation.navigate('Profile', { name: 'Seconed screen' })
-      }/>
-      </View>
-  );
-};
 const SeconedScreen = ({ navigation, route }:any) => {
-  return <Text>This is {route.params.name}</Text>;
+  return <Text>This is seconed Screen,n </Text>;
 };
 
 const App = () => {
   const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+  
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen
-          name="First Screen"
-          component={FirstScreen}
-          options={{ title: 'First Screen' }}
+      <Tab.Navigator
+      screenOptions={({ route }) => ({
+          tabBarActiveTintColor: '#00a3ff',
+          
+      })}
+      >
+      <Tab.Screen
+          name="Home"
+          component={Products}
+          options={{ tabBarBadge: "5" }}
         />
-        <Stack.Screen name="Profile"    options={{ title: 'Seconed Screen' }} component={SeconedScreen} />
-      </Stack.Navigator>
+        <Tab.Screen name="Profile"   options={{ title: 'Seconed Screen' }} component={SeconedScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, flexDirection: 'row', 
+    flexWrap: 'wrap',
+  },
+});
 
 export default App;
