69f8762c0a7615e636870e88f1b12975ac03230b
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2022-01-30 00:23:28 +0530

    Create data.ts

---
+++ utils/data.ts
@@ -0,0 +1,44 @@
+/* eslint-disable eol-last */
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+/* eslint-disable prettier/prettier */
+export  const Data :any = [
+    {
+      id:'item1',
+      imageUrl:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/1376577/2016/9/15/11473928353466-Roadster-Men-Black-Regular-Fit-Checked-Casual-Shirt-4501473928353310-1.jpg",
+      brandName:"RockStar",
+      brandDetails:'Cotton Check T-shirt',
+      brandPrice:'Rs 19999'
+    },
+    {
+      id:'item2',
+      imageUrl:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/2175492/2017/12/12/11513054873776-Kook-N-Keech-Men-Black-Printed-Sweatshirt-51513054873532-1.jpg",
+      brandName:"Kook N Keech",
+      brandDetails:'Cotton Check T-shirt',
+      brandPrice:'Rs 1999'
+    }, {
+      id:'item3',
+      imageUrl:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/15654438/2021/10/5/2f0a5479-f3c3-462c-80e6-fb47902a1bf41633417235878-Computerised-Jacquard-Sweater-1401633417234607-1.jpg",
+      brandName:"ether",
+      brandDetails:'Cotton Check T-shirt',
+      brandPrice:'Rs 19999'
+    }, {
+      id:'item4',
+      imageUrl:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/15654420/2021/10/6/de60b7e8-93e2-4b9f-96d2-635f81909eed1633503008970-Computerised-Jacquard-Sweater-2101633503008471-1.jpg",
+      brandName:"RockStar",
+      brandDetails:'Cotton Check T-shirt',
+      brandPrice:'Rs 19999'
+    }, {
+      id:'item5',
+      imageUrl:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/1377692/2016/11/19/11479544055084-ether-Men-Black-Cardigan-1681479544054839-1.jpg",
+      brandName:"Mast & Harbour",
+      brandDetails:'Cotton Check T-shirt',
+      brandPrice:'Rs 19999'
+    }, {
+      id:'item6',
+      imageUrl:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/1852722/2019/11/27/43eb8691-f198-462e-bcf8-b61131eb19281574858864587-ether-Men-Navy-Self-Design-Sweatshirt-6421574858863197-1.jpg",
+      brandName:"Slazer",
+      brandDetails:'Cotton Check T-shirt',
+      brandPrice:'Rs 199'
+    }
+];
